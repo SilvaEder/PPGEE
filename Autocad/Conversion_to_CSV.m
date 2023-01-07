5ac2d85b9f7647eb2187c56dd5c0adcbb238436a
SilvaEder <ederhenriquesilva@alunos.utfpr.edu.br>
2023-01-07 18:34:59 -0200

    Up .m by windows desktop 07/01/2023

---
+++ Autocad/Conversion_to_CSV.m
@@ -0,0 +1,8 @@
+
+%% Motor 0
+writematrix(data_3,'CSV\motor0_data1.csv');
+writematrix(data_4,'CSV\motor0_data2.csv');
+
+%% Motor 1
+writematrix(data_1,'CSV\motor1_data1.csv');
+writematrix(data_2,'CSV\motor1_data2.csv');
